7cabd48354dabb945f04184d8962563ac20e671e
A. Bram Neijt <bneijt@gmail.com>
2013-05-16 08:16:48 +0200

    Create ip address matcher

--- src/main/java/nl/bneijt/matchmonitor/ApplicationModule.java
+++ src/main/java/nl/bneijt/matchmonitor/ApplicationModule.java
@@ -3,12 +3,12 @@ package nl.bneijt.matchmonitor;
 
 import com.google.inject.AbstractModule;
 import nl.bneijt.matchmonitor.processing.PacketContentsHandler;
-import nl.bneijt.matchmonitor.processing.PacketLetterMatcher;
+import nl.bneijt.matchmonitor.processing.RegexMatcher;
 
 public class ApplicationModule extends AbstractModule {
     @Override
     protected void configure() {
         bind(StateDatabase.class).toInstance(new StateDatabase());
-        bind(PacketContentsHandler.class).to(PacketLetterMatcher.class);
+        bind(PacketContentsHandler.class).to(RegexMatcher.class);
     }
 }
---
+++ src/main/java/nl/bneijt/matchmonitor/processing/RegexMatcher.java
@@ -0,0 +1,26 @@
+package nl.bneijt.matchmonitor.processing;
+
+import com.google.common.base.Splitter;
+import com.google.common.collect.Lists;
+import com.google.inject.Inject;
+import nl.bneijt.matchmonitor.StateDatabase;
+
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class RegexMatcher implements PacketContentsHandler {
+    Pattern pattern;
+    @Inject
+    StateDatabase stateDatabase;
+    public RegexMatcher() {
+        pattern = Pattern.compile("(([0-9]{1,3})\\.([0-9]{1,3})\\.([0-9]{1,3})\\.([0-9]{1,3}))");
+    }
+    @Override
+    public void apply(String content) {
+        Matcher matcher = pattern.matcher(content);
+        if(matcher.find()) {
+            stateDatabase.matched(System.currentTimeMillis(), matcher.group(1));
+        }
+    }
+}
