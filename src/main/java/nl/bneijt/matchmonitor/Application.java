bfa90eccfc69f48cf32966b8ff016dfe54110c53
A. Bram Neijt <bneijt@gmail.com>
2013-05-25 22:08:49 +0200

    Clean up logging output
    New format and back to normal levels

--- src/main/java/nl/bneijt/matchmonitor/Application.java
+++ src/main/java/nl/bneijt/matchmonitor/Application.java
@@ -44,7 +44,8 @@ class Application {
     static final Logger logger;
 
     static {
-        System.setProperty("org.slf4j.simpleLogger.defaultLogLevel", "all");
+        //System.setProperty("org.slf4j.simpleLogger.defaultLogLevel", "all");
+        System.setProperty("java.util.logging.SimpleFormatter.format","%1$tY-%1$tm-%1$td %1$tH:%1$tM:%1$tS %4$s %5$s%6$s%n");
         logger = LoggerFactory.getLogger(Application.class);
     }
 
